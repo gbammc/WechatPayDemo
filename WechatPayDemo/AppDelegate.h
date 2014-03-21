d8fc8ae8d2e92595895f0a0481da33f3f2aed71b
gbammc <gbammc@gmail.com>
2014-03-22 01:46:38 +0800

    Initial Commit

---
+++ WechatPayDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WechatPayDemo
+//
+//  Created by Alvin on 3/22/14.
+//  Copyright (c) 2014 Alvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WechatPayDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WechatPayDemo
+//
+//  Created by Alvin on 3/22/14.
+//  Copyright (c) 2014 Alvin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
